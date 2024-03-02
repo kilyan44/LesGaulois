fdacd5e84aa7e7d974d0ccaf896a72bfd83e808f
kilyan44 <k.delhoume@gmail.com>
2024-03-02 17:06:30 +0100

    fin 1 TP2

--- src/personnages/romain.java
+++ src/personnages/romain.java
@@ -3,9 +3,11 @@ package personnages;
 public class romain {
 	private String nom;
 	private int force;
+	private int stockageForce;
 
 	public romain(String nom, int force) {
 		this.nom = nom;
+		assert forceRomainPositive(force);
 		this.force = force;
 	}
 
@@ -21,19 +23,37 @@ public class romain {
 		return "Le romain " + nom + " : ";
 	}
 
+	private boolean forceRomainPositive(int force) {
+		if (force <= 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	private boolean forceRomainDiminuer(int forceAvant, int forceApres) {
+		if (forceAvant < forceApres) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 	public void recevoirCoup(int forceCoup) {
+		assert forceRomainPositive(force);
+		stockageForce = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert forceRomainDiminuer(stockageForce, force);
 	}
 
 	public static void main(String[] args) {
-		romain romain = new romain("obelix", 9);
+		romain romain = new romain("minus", 6);
 		System.out.println(romain);
-		System.out.println(romain.prendreParole());
 		romain.parler("Bonjour");
 		romain.recevoirCoup(1);
 	}
